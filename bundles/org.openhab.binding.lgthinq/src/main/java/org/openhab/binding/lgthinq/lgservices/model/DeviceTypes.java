3050f65f9d97063bd969ad336fed2f4d7de453b4
Nemer Daud <nemer.daud@gmail.com>
2022-02-03 12:19:08 -0300

    [lgthinq] Repackaging, Enhanced data binding and some logs for troubleshooting Signed-off-by: nemerdaud <nemer.daud@gmail.com>

--- bundles/org.openhab.binding.lgthinq/src/main/java/org/openhab/binding/lgthinq/lgservices/model/DeviceTypes.java
+++ bundles/org.openhab.binding.lgthinq/src/main/java/org/openhab/binding/lgthinq/lgservices/model/DeviceTypes.java
@@ -18,79 +18,25 @@ package org.openhab.binding.lgthinq.lgservices.model;
  * @author Nemer Daud - Initial contribution
  */
 public enum DeviceTypes {
-    AIR_CONDITIONER(401, "ACCapability", ""),
-
-    HEAT_PUMP(401, "ACCapability", "AWHP"),
-    WASHERDRYER_MACHINE(201, "WM", ""),
-
-    WASHING_TOWER(221, "WM", ""),
-    DRYER(202, "DR", "Dryer"),
-    DRYER_TOWER(222, "DR", "Dryer"),
-    REFRIGERATOR(101, "REF", "Fridge"),
-    UNKNOWN(-1, "", "");
+    AIR_CONDITIONER(401),
+    UNKNOWN(-1);
 
     private final int deviceTypeId;
-    private final String deviceTypeAcron;
-    private final String deviceSubModel;
-
-    public String deviceTypeAcron() {
-        return deviceTypeAcron;
-    }
 
     public int deviceTypeId() {
         return deviceTypeId;
     }
 
-    public String deviceSubModel() {
-        return deviceSubModel;
-    }
-
-    public static DeviceTypes fromDeviceTypeId(int deviceTypeId, String deviceCode) {
+    public static DeviceTypes fromDeviceTypeId(int deviceTypeId) {
         switch (deviceTypeId) {
             case 401:
-                if ("AI05".equals(deviceCode)) {
-                    return HEAT_PUMP;
-                }
                 return AIR_CONDITIONER;
-            case 201:
-                return WASHERDRYER_MACHINE;
-            case 221:
-                return WASHING_TOWER;
-            case 202:
-                return DRYER;
-            case 222:
-                return DRYER_TOWER;
-            case 101:
-                return REFRIGERATOR;
             default:
                 return UNKNOWN;
         }
     }
 
-    public static DeviceTypes fromDeviceTypeAcron(String deviceTypeAcron, String modelType) {
-        switch (deviceTypeAcron) {
-            case "AC":
-                if ("RAC".equals(modelType)) {
-                    return AIR_CONDITIONER;
-                } else if ("AWHP".equals(modelType)) {
-                    return HEAT_PUMP;
-                }
-            case "WM":
-                if ("Dryer".equals(modelType)) {
-                    return DRYER;
-                } else {
-                    return WASHERDRYER_MACHINE;
-                }
-            case "REF":
-                return REFRIGERATOR;
-            default:
-                return UNKNOWN;
-        }
-    }
-
-    DeviceTypes(int i, String n, String submodel) {
+    DeviceTypes(int i) {
         this.deviceTypeId = i;
-        this.deviceTypeAcron = n;
-        this.deviceSubModel = submodel;
     }
 }
